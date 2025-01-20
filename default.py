39748a727562e3cc753a086556761152550c709d
idealkooks <163488855+thoffical@users.noreply.github.com>
2025-01-20 11:43:31 +0800

    Update default.py

--- default.py
+++ default.py
@@ -8,6 +8,12 @@ class FileMote:
     def __init__(self):
         self.addon = xbmcaddon.Addon(id='script.thapps.filemote')
         self.user_ip = self.addon.getSetting('user_ip')
+        
+        # Check if user_ip is set
+        if not self.user_ip:
+            xbmcgui.Dialog().ok("Error", "Please set the User IP Address in the addon settings.")
+            return
+        
         self.show_main_menu()
 
     def show_main_menu(self):
@@ -20,9 +26,7 @@ class FileMote:
 
     def manage_files(self, platform):
         xbmcgui.Dialog().notification("FileMote", f"You selected to manage files on {platform}.", xbmcgui.NOTIFICATION_INFO, 5000)
-
-        if self.user_ip:
-            self.connect_to_server(self.user_ip)
+        self.connect_to_server(self.user_ip)
 
     def connect_to_server(self, address):
         try:
@@ -30,6 +34,7 @@ class FileMote:
             port = int(port)
 
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+                sock.settimeout(5)  # Set a timeout of 5 seconds
                 sock.connect((host, port))
                 command = json.dumps({"action": "list_files"})
                 sock.sendall(command.encode('utf-8'))
@@ -42,7 +47,11 @@ class FileMote:
     def handle_server_response(self, response):
         try:
             data = json.loads(response)
-            xbmcgui.Dialog().notification("Server Response", str(data), xbmcgui.NOTIFICATION_INFO, 5000)
+            if isinstance(data, list):
+                file_list = "\n".join(data)  # Assuming the response is a list of files
+                xbmcgui.Dialog().ok("Server Response", f"Files:\n{file_list}")
+            else:
+                xbmcgui.Dialog().notification("Error", "Unexpected response format.", xbmcgui.NOTIFICATION_ERROR, 5000)
         except json.JSONDecodeError:
             xbmcgui.Dialog().notification("Error", "Failed to decode server response.", xbmcgui.NOTIFICATION_ERROR, 5000)
 
